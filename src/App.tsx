dbffe15ef5abf1b29ffdbaa47b4b36148a19dd49
Oke Amuwha <amuwhao46@gmail.com>
2024-10-05 07:34:43 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center align-middle">
+        <p className='text-green-500 font-bold'>Testing</p>
+      </div>
+    </>
+  )
+}
+
+export default App
